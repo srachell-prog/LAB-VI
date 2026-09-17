44756281fe310e40058d0eeece626f4e6c8bf9a6
schachell <rdepazs1@miumg.edu.gt>
2026-09-17 00:37:56 -0600

    Se agrega estructura base del proyecto

---
+++ src/main/java/com/rachell/laboratorio_VI/LaboratorioViApplication.java
@@ -0,0 +1,13 @@
+package com.rachell.laboratorio_VI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratorioViApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratorioViApplication.class, args);
+	}
+
+}
